6361ea42b62a23cb61d60aec5059d76085f8efb4
Rakesh-Biswal <rb2306114@gmail.com>
2024-07-31 20:07:14 +0530

    gvfcdxcfvg

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 5500;
+
+const mongoURI = 'mongodb+srv://pramod18:pramodB18@cluster0.cny7fgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Jarvis1.0';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const interactionSchema = new mongoose.Schema({
+    speaker: String,
+    message: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+const Interaction = mongoose.model('Interaction', interactionSchema);
+
+app.post('/interactions', async (req, res) => {
+    const { speaker, message } = req.body;
+    const interaction = new Interaction({ speaker, message });
+    try {
+        await interaction.save();
+        res.status(201).send(interaction);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.get('/interactions', async (req, res) => {
+    try {
+        const interactions = await Interaction.find();
+        res.status(200).send(interactions);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
